856b0727f5cd231ff027a33d939d3b05757b5b35
yusufali19 <yusufali9398@gmail.com>
2022-01-15 11:07:46 +0530

    add logic for ownership

--- api/job/controllers/job.js
+++ api/job/controllers/job.js
@@ -1,29 +1,43 @@
-const { sanitizeEntity } = require("strapi-utils");
-const jwt = require("jsonwebtoken");
+const { sanitizeEntity, parseMultipartData } = require("strapi-utils");
 
 module.exports = {
   /**
-   * Retrieve records.
+   * Create a record.
+   *
+   * @return {Object}
+   */
+  async create(ctx) {
+    let entity;
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.owner = ctx.state.user.id;
+      entity = await strapi.services.job.create(data, { files });
+    } else {
+      ctx.request.body.owner = ctx.state.user.id;
+      entity = await strapi.services.job.create(ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.job });
+  },
+
+  /**
+   * Find Owner record.
    *
    * @return {Array}
    */
-
   async findMy(ctx) {
     let entities;
 
+    const query = {
+      ...ctx.query,
+      "owner.id": ctx.state.user.id,
+    };
+
     if (ctx.query._q) {
-      entities = await strapi.services.job.search(ctx.query);
+      entities = await strapi.services.job.search(query);
     } else {
-      entities = await strapi.services.job.find(ctx.query);
-    }
-
-    // Set entities based on current authenticated user.
-    if (ctx.header?.authorization) {
-      const authorization = ctx.header?.authorization ?? "";
-      const jwtToken = authorization.split(" ")?.[1];
-      const decoded = jwt.decode(jwtToken);
-
-      entities = entities.filter((entity) => entity?.user?.id === decoded.id);
+      entities = await strapi.services.job.find(query);
     }
 
     return entities.map((entity) =>
